92430966c9f332f9610453f8f5bc3260f4e75bb0
ndkv9 <vu.nguyen101@gmail.com>
2021-11-03 20:38:31 +0200

    initial setup

---
+++ CodingTestLibrary/PersonModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodingTestLibrary
+{
+  public class PersonModel
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+  }
+}
---
+++ CodingTestUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingTestUI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!\n...and Turku!");
+    }
+  }
+}
